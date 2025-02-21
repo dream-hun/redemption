c8ad264f613a0e19da7c94d1243aa9e46a3c4797
MBABAZI Jacques <71966425+dream-hun@users.noreply.github.com>
2025-02-21 08:40:19 +0200

    initial commit

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -24,7 +24,19 @@
             <div class="hidden sm:flex sm:items-center sm:ms-6">
                 <!-- Shopping Cart Link -->
                 <x-nav-link :href="route('cart.index')" :active="request()->routeIs('cart.index')" class="me-4">
-                    {{ __('Shopping Cart') }}
+                    <span class="relative inline-block">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                            stroke="currentColor" class="size-6">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
+                        </svg>
+
+                        @if ($cartCount > 0)
+                            <span
+                                class="absolute top-0 right-0 inline-flex items-center justify-center w-6 h-6 text-xs font-bold leading-none text-white transform translate-x-1/2 -translate-y-1/2 bg-red-600 rounded-full">{{ Cart::instance('default')->count() }}</span>
+                        @endif
+                    </span>
+
                 </x-nav-link>
 
                 <!-- Settings Dropdown -->
