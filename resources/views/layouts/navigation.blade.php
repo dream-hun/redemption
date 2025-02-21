d26a95910d1fc09d135409a88adb584fb0f2ea56
MBABAZI Jacques <71966425+dream-hun@users.noreply.github.com>
2025-02-21 09:03:21 +0200

    fix cartcount

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -33,7 +33,7 @@
 
                         @if ($cartCount > 0)
                             <span
-                                class="absolute top-0 right-0 inline-flex items-center justify-center w-6 h-6 text-xs font-bold leading-none text-white transform translate-x-1/2 -translate-y-1/2 bg-red-600 rounded-full">{{ $cartCount }}</span>
+                                class="absolute top-0 right-0 inline-flex items-center justify-center w-6 h-6 text-xs font-bold leading-none text-black transform translate-x-1/2 -translate-y-1/2 bg-blue-600 rounded-full">{{ $cartCount }}</span>
                         @endif
                     </span>
 
